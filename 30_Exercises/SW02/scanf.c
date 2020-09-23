15d915303083da9b575a824991e8c4d6808ea8d1
francesco <francesco.planta@stud.hslu.ch>
2020-09-23 09:26:03 +0200

    SW02

---
+++ 30_Exercises/SW02/scanf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{	
+	int readValue = 0;
+
+	printf("Enter Numner:");
+	scanf("%d", &readValue);
+	printf("Enteret Value: %d\n", readValue);
+
+
+
+
+
+
+
+
+
+	return 0;
+}
