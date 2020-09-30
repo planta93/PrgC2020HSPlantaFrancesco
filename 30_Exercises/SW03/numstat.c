ec70d55b327880f2c60a24881b15aa3e40f2e47f
francesco <francesco.planta@stud.hslu.ch>
2020-09-30 15:41:22 +0200

    numstat.1

--- 30_Exercises/SW03/numstat.c
+++ 30_Exercises/SW03/numstat.c
@@ -2,41 +2,51 @@
 #include <stdlib.h>
 
 
-int main(int argc, char *argv[]	)
+int main( int argc, char *argv[]	)
 {
 	
 	float inputValue1 = 0, inputValue2 = 0;
-	printf("Number of input arguments: %d\n", argc);
-	printf("Insert two floats:\n");
-	scanf("%f\n%f", &inputValue1, &inputValue2);
-	printf("Number of input arguments: %d\n", argc);
 	
-	if (inputValue1 >= inputValue2)
+	
+	
+	
+	
+	if (argc == 1)
 	{
-		printf("Input1:\t\t%.3f >= Input2:\t%.3f\n", inputValue1, inputValue2);
-		printf("Sum:\t\t%.3f + %.3f= %.3f\n", inputValue1, inputValue2, inputValue1+inputValue2);
-		printf("Difference:\t%.3f - %.3f= %.3f\n", inputValue1, inputValue2, inputValue1-inputValue2);
-		printf("Product:\t%.3f * %.3f= %.3f\n", inputValue1, inputValue2, inputValue1*inputValue2);
-		printf("Ratio:\t\t%.3f / %.3f= %.3f\n", inputValue2, inputValue1, inputValue2/inputValue1);
+		printf("Number of input arguments: %d\n", argc);
+		printf("Insert two floats:\n");
+		scanf("%f\n%f", &inputValue1, &inputValue2);
+		printf("Number of input arguments: %d\n", argc);
+		
+		if (inputValue1 >= inputValue2)
+		{
+			printf("Input1:\t\t%.3f >= Input2:\t%.3f\n", inputValue1, inputValue2);
+			printf("Sum:\t\t%.3f + %.3f= %.3f\n", inputValue1, inputValue2, inputValue1+inputValue2);
+			printf("Difference:\t%.3f - %.3f= %.3f\n", inputValue1, inputValue2, inputValue1-inputValue2);
+			printf("Product:\t%.3f * %.3f= %.3f\n", inputValue1, inputValue2, inputValue1*inputValue2);
+			printf("Ratio:\t\t%.3f / %.3f= %.3f\n", inputValue2, inputValue1, inputValue2/inputValue1);
+	
+		}
+		
+		
+		
+		else
+		{
+			printf("Input1:\t\t%.3f <=  Input2:%.3f\n", inputValue1, inputValue2);
+			printf("Sum:\t\t%.3f + %.3f= %.3f\n", inputValue2, inputValue1, inputValue2+inputValue1);
+			printf("Difference:\t%.3f - %.3f= %.3f\n", inputValue2, inputValue1, inputValue2-inputValue1);
+			printf("Product:\t%.3f * %.3f= %.3f\n", inputValue2, inputValue1, inputValue2*inputValue1);
+			printf("Ratio:\t\t%.3f / %.3f= %.3f\n", inputValue1, inputValue2, inputValue1/inputValue2);
 
+		}
+
+	
 	}
 	
-	
-	
-	else
+	else 
 	{
-		printf("Input1:\t\t%.3f <=  Input2:%.3f\n", inputValue1, inputValue2);
-		printf("Sum:\t\t%.3f + %.3f= %.3f\n", inputValue2, inputValue1, inputValue2+inputValue1);
-		printf("Difference:\t%.3f - %.3f= %.3f\n", inputValue2, inputValue1, inputValue2-inputValue1);
-		printf("Product:\t%.3f * %.3f= %.3f\n", inputValue2, inputValue1, inputValue2*inputValue1);
-		printf("Ratio:\t\t%.3f / %.3f= %.3f\n", inputValue1, inputValue2, inputValue1/inputValue2);
-
+		printf("usage message: RTFI!\n");
 	}
-
-	printf("Number of input arguments: %d\n", argc);
-	
-
-
 
 return 0;
 }
