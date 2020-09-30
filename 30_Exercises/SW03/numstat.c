ae26383cf535e4162c358b8423be96116393a019
francesco <francesco.planta@stud.hslu.ch>
2020-09-30 16:31:36 +0200

    numstat.2

--- 30_Exercises/SW03/numstat.c
+++ 30_Exercises/SW03/numstat.c
@@ -5,22 +5,21 @@
 int main( int argc, char *argv[]	)
 {
 	
-	float inputValue1 = 0, inputValue2 = 0;
+	float inputValue1 = atof(argv[1]), inputValue2 = atof(argv[2]);
 	
 	
 	
 	
 	
-	if (argc == 1)
+	if (argc == 3)
 	{
-		printf("Number of input arguments: %d\n", argc);
-		printf("Insert two floats:\n");
-		scanf("%f\n%f", &inputValue1, &inputValue2);
-		printf("Number of input arguments: %d\n", argc);
+		//scanf("%f\n%f", &inputValue1, &inputValue2);
+		//printf("Number of input arguments: %d\n", argc);
+		//printf("%s, %s, %s ", argv[0], argv[1], argv[2]);
 		
 		if (inputValue1 >= inputValue2)
 		{
-			printf("Input1:\t\t%.3f >= Input2:\t%.3f\n", inputValue1, inputValue2);
+			printf("Input1:\t\t%.3f >= Input2:%.3f\n", inputValue1, inputValue2);
 			printf("Sum:\t\t%.3f + %.3f= %.3f\n", inputValue1, inputValue2, inputValue1+inputValue2);
 			printf("Difference:\t%.3f - %.3f= %.3f\n", inputValue1, inputValue2, inputValue1-inputValue2);
 			printf("Product:\t%.3f * %.3f= %.3f\n", inputValue1, inputValue2, inputValue1*inputValue2);
@@ -45,7 +44,7 @@ int main( int argc, char *argv[]	)
 	
 	else 
 	{
-		printf("usage message: RTFI!\n");
+		printf("Usage message: Please insert two floats!\n");
 	}
 
 return 0;
