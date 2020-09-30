8fcf1ef735cb5f8fc767513b20a409d87eada351
francesco <francesco.planta@stud.hslu.ch>
2020-09-30 15:13:35 +0200

    numstat

---
+++ 30_Exercises/SW03/numstat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]	)
+{
+	
+	float inputValue1 = 0, inputValue2 = 0;
+	printf("Number of input arguments: %d\n", argc);
+	printf("Insert two floats:\n");
+	scanf("%f\n%f", &inputValue1, &inputValue2);
+	printf("Number of input arguments: %d\n", argc);
+	
+	if (inputValue1 >= inputValue2)
+	{
+		printf("Input1:\t\t%.3f >= Input2:\t%.3f\n", inputValue1, inputValue2);
+		printf("Sum:\t\t%.3f + %.3f= %.3f\n", inputValue1, inputValue2, inputValue1+inputValue2);
+		printf("Difference:\t%.3f - %.3f= %.3f\n", inputValue1, inputValue2, inputValue1-inputValue2);
+		printf("Product:\t%.3f * %.3f= %.3f\n", inputValue1, inputValue2, inputValue1*inputValue2);
+		printf("Ratio:\t\t%.3f / %.3f= %.3f\n", inputValue2, inputValue1, inputValue2/inputValue1);
+
+	}
+	
+	
+	
+	else
+	{
+		printf("Input1:\t\t%.3f <=  Input2:%.3f\n", inputValue1, inputValue2);
+		printf("Sum:\t\t%.3f + %.3f= %.3f\n", inputValue2, inputValue1, inputValue2+inputValue1);
+		printf("Difference:\t%.3f - %.3f= %.3f\n", inputValue2, inputValue1, inputValue2-inputValue1);
+		printf("Product:\t%.3f * %.3f= %.3f\n", inputValue2, inputValue1, inputValue2*inputValue1);
+		printf("Ratio:\t\t%.3f / %.3f= %.3f\n", inputValue1, inputValue2, inputValue1/inputValue2);
+
+	}
+
+	printf("Number of input arguments: %d\n", argc);
+	
+
+
+
+return 0;
+}
